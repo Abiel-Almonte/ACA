93c466dd3c1eb91355cac59dd446bee9b6883c9b
Abiel Almonte <almonteabiel@gmail.com>
2024-07-16 22:59:19 -0400

    chore: removed consts not used

--- server/logic/const.py
+++ server/logic/const.py
@@ -1,12 +1,6 @@
 import os
 
-
-HOST= 'localhost'
-PORT= 19530
-USER= 'root'
-PASS= 'Milvus'
 BERT_MODEL='distilbert/distilbert-base-uncased'
 BERT_DIM= 768
-CONNECTION_ARGS= {'host': HOST, 'port': PORT, 'user': USER, 'password': PASS}
 DATA1= os.path.join(os.path.dirname(__file__), '../data/cis.pq')
 DATA2= os.path.join(os.path.dirname(__file__), '../data/ir.pq')
